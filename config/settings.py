120fa813ec74bd2e1049253ea22e483d70e2d36a
Muhammad Ali <xaydarovmuhammadali350@gmail.com>
2026-08-30 17:32:07 +0500

    Fix media upload: auto-create media directory + fix image URLs

--- config/settings.py
+++ config/settings.py
@@ -95,6 +95,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+os.makedirs(MEDIA_ROOT, exist_ok=True)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
